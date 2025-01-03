23f0af8aab3ee091f0868237c320c506aefdde99
JJ Brown <jj.brown.255@proton.me>
2025-01-02 22:34:49 -0700

    Actually reading the uxplay version

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use glib::clone;
 use gtk::glib;
 use gtk::prelude::*;
 
+use which::which;
+
+use std::process::Command;
+
 // When the application is launched…
 fn on_activate(application: &gtk::Application) {
     // … create a new window …
@@ -18,6 +22,16 @@ fn on_activate(application: &gtk::Application) {
         .build();
 
     let header = gtk::Label::new(Some("UxPlay Status:"));
+    let status_label = gtk::Label::new(Some(" Loading..."));
+    window.connect_show(clone!(@weak status_label => move |_| {
+        let version = get_uxplay_version();
+        let version_str: String = match version {
+            NotInstalled => String::from("Not installed or not in $PATH"),
+            ErrorRunning => String::from("Installed but could not be run"),
+            Installed{ version: v } => format!("Installed: {}", v),
+        };
+        status_label.set_label(&version_str);
+    }));
 
     let button = gtk::Button::with_label("Close");
 
@@ -30,6 +44,7 @@ fn on_activate(application: &gtk::Application) {
         .build();
 
     the_box.append(&header);
+    the_box.append(&status_label);
     the_box.append(&button);
 
     // … which closes the window when clicked
@@ -47,3 +62,34 @@ fn main() {
     // Run the application
     app.run();
 }
+
+enum UxPlayInstallStatus {
+    NotInstalled,
+    ErrorRunning,
+    Installed { version: String },
+}
+
+use UxPlayInstallStatus::*;
+
+fn get_uxplay_version() -> UxPlayInstallStatus {
+    let uxplay_path = match which("uxplay") {
+        Ok(p) => p,
+        Err(_) => return NotInstalled,
+    };
+
+    let output = Command::new(&uxplay_path)
+        .arg("-v")
+        .output();
+
+    let output = match output {
+        Ok(out) => String::from_utf8(out.stdout),
+        Err(_) => return ErrorRunning,
+    };
+
+    match output {
+        Ok(string) => Installed{version: string},
+        Err(_) => return ErrorRunning,
+    }
+}
+
+
